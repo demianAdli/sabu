a8e560ee03d9ea9c127f5718c871fd6eb33ffe5e
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-04-01 16:46:46 -0400

    Add the input paths

--- services/jug_gis_cities/src/jug_gis_cities/saint_malachie_gisoo/input_paths_and_layers.py
+++ services/jug_gis_cities/src/jug_gis_cities/saint_malachie_gisoo/input_paths_and_layers.py
@@ -23,11 +23,18 @@ def create_output_folders(paths_dict, output_dir):
 # Application's path
 qgis_path = 'C:/Program Files/QGIS 3.34.1/apps/qgis'
 
-# Gathering input data layers paths
+# Gathering input data layers paths 
 input_paths = {
-  'ROLE 2025':
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/input_data/'
-  'cerc_district_111/layer_111.shp'
+  'qc_property_roll_2025':
+  'D:/GIS/saint_malachie_gisoo_data/input_data/'
+  'saint_malachie_role_2025_unit_eval_et_address_geopackage/'
+  'role_2025_unit_eval_et_address_geopackage_quebec.gpkg',
+  'nrcan_heights':
+  'D:/GIS/saint_malachie_gisoo_data/input_data/'
+  'saint_malachie_auto_with_heights/saint_malachie_auto_with_heights.gpkg',
+  'qc_geoindex':
+  'D:/GIS/saint_malachie_gisoo_data/input_data/saint_claude_mahm_usage_2025/'
+  'mamh_usage_predo_2025_s_poly.shp'
 }
 
 # Defining a directory for all the output data layers
