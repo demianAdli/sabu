78b495c1575478facdd819aaae8455abff66b385
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-04-02 14:52:51 -0400

    cities: write a template

--- services/jug_gis_cities/src/jug_gis_cities/saint_claude_gisoo/handle_saint_claude_wf.py
+++ services/jug_gis_cities/src/jug_gis_cities/saint_claude_gisoo/handle_saint_claude_wf.py
@@ -14,8 +14,37 @@ from jug_gis_cities.saint_claude_gisoo import input_paths_and_layers as paths
 # Making folders for the output data layers
 paths.create_output_folders(paths.output_paths, paths.output_paths_dir)
 
-cerc_boundary = \
+property_roll = \
   ScrubLayer(
-    paths.qgis_path,
-    paths.input_paths['CERC Boundary'],
-    'CERC Boundary')
+    paths.qgis_path, paths.input_paths['qc_property_roll_2025'], 'roll')
+
+nrcan = \
+  ScrubLayer(paths.qgis_path, paths.input_paths['nrcan_heights'], 'nrcan')
+
+geoindex = \
+  ScrubLayer(paths.qgis_path, paths.input_paths['qc_geoindex'], 'geoindex')
+
+# Processing the NRCan layer includes fixing its geometries
+print('Processing the NRCan layer')
+print(nrcan)
+nrcan.create_spatial_index()
+nrcan.fix_geometries(paths.output_paths['nrcan_fixed'])
+
+# Defining a new layer for the fixed NRCan
+nrcan_fixed = \
+  ScrubLayer(paths.qgis_path, paths.output_paths['nrcan_fixed'], 'nrcan_fixed')
+nrcan_fixed.create_spatial_index()
+print(nrcan_fixed)
+
+# Processing the geoindex layer includes fixing its geometries
+print('Processing the GeoIndex layer')
+print(geoindex)
+geoindex.create_spatial_index()
+geoindex.fix_geometries(paths.output_paths['geoindex_fixed'])
+
+# Defining a new layer for the fixed GeoIndex
+geoindex_fixed = ScrubLayer(
+  paths.qgis_path, paths.output_paths['Fixed GeoIndex'], 'geoindex_fixed')
+geoindex_fixed.create_spatial_index()
+print(geoindex_fixed)
+
